0104117932d39587cf33f777bd283c0c335f22c5
Dnyandeep <dnyandeepc18@gmail.com>
2023-10-16 01:56:28 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+# def get_requirements(file_path: str)->List[str]:
+#     '''
+#     This fucntion will return the list of requirements
+#     ''' 
+#     requirements = []
+
+#     with open(file_path) as file:
+
+
+setup(
+    name='Livesitter_MLops',
+    version='0.0.1',
+    author='Dnyandeep',
+    author_email='dnyandeepc18@gmail.com',
+    packages= find_packages()
+)
